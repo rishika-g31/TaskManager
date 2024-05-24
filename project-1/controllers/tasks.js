1bfd4a7329de516d1fca7247b967319921e2bc11
rishika-g31 <rishikagupta312004@gmail.com>
2024-05-25 01:26:29 +0530

    added validators

--- project-1/controllers/tasks.js
+++ project-1/controllers/tasks.js
@@ -1,19 +1,66 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("all items");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json;
+    ({ msg: error });
+  }
 };
 
-const createTasks = (req, res) => {
-  res.json(req.body);
+const createTasks = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json;
+    ({ msg: error });
+  }
 };
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    res.status(200).json({ task });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID} ` });
+    }
+  } catch (error) {
+    res.status(500).json;
+    ({ msg: error });
+  }
 };
-const updateTask = (req, res) => {
-  res.send("update Task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json;
+    ({ msg: error });
+  }
 };
-const deleteTasks = (req, res) => {
+const deleteTasks = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json;
+    ({ msg: error });
+  }
   res.send("delete Task");
 };
 
